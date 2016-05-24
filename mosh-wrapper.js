975c5b2f20c724f1429173da7ec868ada2355d69
Greg Cochard <greg.cochard@gmail.com>
2016-05-24 11:17:31 -0700

    Fix regex issue for encryption key

--- mosh-wrapper.js
+++ mosh-wrapper.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 var exec = require('child_process').exec;
 var fs = require('fs');
+var util = require('util');
+var debug = util.debuglog('mosh');
 function getIp(){
   var interfaces = require('os').networkInterfaces();
   for (var devName in interfaces) {
@@ -14,13 +16,17 @@ function getIp(){
   return null;
 }
 
+debug('ip: %s', getIp());
+
 exec('mosh-server new', function(err,stdout,stderr){
     // output has 1 newline and then the MOSH CONNECT string
-    var re = /^MOSH CONNECT (6\d{4}) ([a-zA-Z0-9]+)$/;
+    var re = /^MOSH CONNECT (6\d{4}) ([!-~]+)$/;
     var matches = stdout.split('\n').map(function(s){ var m = s.match(re); return m?m[1]:''; }).join('');
+    debug('matches: %j', matches);
     if(matches){
       var port = matches;
       var ip = getIp();
+      debug('port: %s, ip: %s',port, ip);
       if(ip){
         // this happens async, but works quickly enough to not be a big deal
         // mosh just picks up when the hole is punched
